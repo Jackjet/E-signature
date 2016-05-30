93080d16299b7bca84cab2fcc24123e27cbd7629
Wastrel <907972944@qq.com>
2016-05-30 16:30:29 +0800

    优化代码

--- app/src/main/java/com/example/ksi_android/handwritedemo/HandWriteActivity.java
+++ app/src/main/java/com/example/ksi_android/handwritedemo/HandWriteActivity.java
@@ -3,6 +3,7 @@ package com.example.ksi_android.handwritedemo;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -13,17 +14,17 @@ import butterknife.ButterKnife;
 public class HandWriteActivity extends AppCompatActivity {
 
     @Bind(R.id.view)
-    LinePathView view;
+    LinePathView mPathView;
     @Bind(R.id.clear1)
-    Button mclear;
+    Button mClear;
     @Bind(R.id.ll)
     LinearLayout ll;
     @Bind(R.id.save1)
-    Button msave;
+    Button mSave;
     @Bind(R.id.change)
-    Button changecolor;
+    Button mChangeColor;
     @Bind(R.id.changewidth)
-    Button changewidth;
+    Button mChangeWidth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +32,45 @@ public class HandWriteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hand_write);
         ButterKnife.bind(this);
         setResult(50);
+        //设置保存监听
+        mSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPathView.getTouched()) {
+                    mPathView.save(MainActivity.path, true, 10, true, 0.9);
+                    setResult(100);
+                    finish();
+                } else {
 
-        msave.setOnClickListener(v -> {
-            if (view.getTouched()) {
-                view.save(MainActivity.path, true, 10, true, 0.9);
-                setResult(100);
-                finish();
-            } else {
-
-                Toast.makeText(HandWriteActivity.this, "您没有签名~", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(HandWriteActivity.this, "您没有签名~", Toast.LENGTH_SHORT).show();
+                }
             }
-
         });
-        mclear.setOnClickListener(v -> view.clear());
-        changecolor.setOnClickListener(v->{view.setmBackColor(Color.RED);view.setmPenColor(Color.WHITE);view.clear();});
-        changewidth.setOnClickListener(v->{view.setmPaintWidth(20);view.clear();});
+        mClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPathView.clear();
+            }
+        });
+        //修改背景颜色和笔的颜色
+        mChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                mPathView.setBackColor(Color.RED);
+                mPathView.setPenColor(Color.WHITE);
+                mPathView.clear();
+            }
+        });
+        //修改笔的宽度
+        mChangeWidth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                mPathView.setPaintWidth(20);
+                mPathView.clear();
+            }
+        });
     }
 
     @Override
